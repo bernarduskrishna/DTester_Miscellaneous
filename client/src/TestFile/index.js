45c17412ee6cad1ee758bf5c495ce187833a30bd
bernarduskrishna <krishnabernardus@gmail.com>
2024-07-01 10:11:16 +0700

    Add toggle whether to show the full test or not

--- client/src/TestFile/index.js
+++ client/src/TestFile/index.js
@@ -1,10 +1,12 @@
 import "./index.css";
+import { useState } from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function TestFile({testFile, currTest, labelNoFocalMethod, data, focalFilePath, currTestIndex, labelLastLabel, manualLabel}) {
   const startLine = currTest && currTest[0];
   const endLine = currTest && currTest[1];
+  const [showFullTest, setShowFullTest] = useState(true);
   return (
     <div className="test_file">
       <div className="horizontalFlex">
@@ -20,8 +22,11 @@ function TestFile({testFile, currTest, labelNoFocalMethod, data, focalFilePath, 
         {testFile.slice(startLine, endLine).join('')}
       </SyntaxHighlighter>}
       <br/>
-      <h4>Full test file</h4>
-      {testFile && <SyntaxHighlighter language="java" theme={docco} wrapLongLines={true}>
+      <div className="horizontalFlex">
+        <h4>Full test file</h4>
+        <button onClick={() => setShowFullTest((prev) => !prev)}>Show full test</button>
+      </div>
+      {testFile && showFullTest && <SyntaxHighlighter language="java" theme={docco} wrapLongLines={true}>
         {testFile.join('')}
       </SyntaxHighlighter>}
     </div>
